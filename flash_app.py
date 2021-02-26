7297f44fec56123bee5b9d1de184067cf74a2808
alfonsogildegea <alfonsogildegea@gmail.com>
2021-02-26 10:47:52 +0100

    t1

---
+++ flash_app.py
@@ -0,0 +1,8 @@
+# A very simple Flask Hello World app for you to get started with...
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask <br /> <b> HOLA! </b>'
